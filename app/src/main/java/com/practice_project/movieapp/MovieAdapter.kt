000eb53f9b373b7b205a795a3a8e39ee3c8183af
Justin Gent <justinbgent@gmail.com>
2020-07-31 17:39:03 -0600

    Add a recyclerView to popular_movies.xml, setup recycler_item.xml, and MovieAdapter.kt for it

---
+++ app/src/main/java/com/practice_project/movieapp/MovieAdapter.kt
@@ -0,0 +1,23 @@
+package com.practice_project.movieapp
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MovieAdapter(): RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
