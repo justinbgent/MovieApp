0ae9092fffbc11d33b18ba8a7aeb36019a0e8085
Justin Gent <justinbgent@gmail.com>
2020-08-01 16:38:23 -0600

    Move searchMovies method from PopularViewModel.kt to MainViewModel.kt to enable the popular movie list to persist longer

--- app/src/main/java/com/practice_project/movieapp/viewmodel/PopularViewModel.kt
+++ app/src/main/java/com/practice_project/movieapp/viewmodel/PopularViewModel.kt
@@ -11,24 +11,15 @@ import io.reactivex.schedulers.Schedulers
 
 class PopularViewModel: ViewModel() {
     private var disposable: Disposable? = null
-    private var movieList = MutableLiveData<MovieList>()
-    val liveMovieList: LiveData<MovieList> = movieList
-
-    fun getPopularMovies(movieService: MovieService, page: Int = 1) {
-        disposable = movieService.getPopularMovies(page)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe{ movies ->
-                movieList.value = movies
-            }
-    }
+    private var searchMovies = MutableLiveData<MovieList>()
+    val searchMovieList: LiveData<MovieList> = searchMovies
 
     fun searchMovies(search: String, movieService: MovieService, page: Int = 1) {
         disposable = movieService.searchMovies(search, page)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe{ movies ->
-                movieList.value = movies
+                searchMovies.value = movies
             }
     }
 
