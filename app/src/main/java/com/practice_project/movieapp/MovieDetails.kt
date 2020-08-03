04ddcbb674bd1466b3adcf8d67cb014faf1ce458
Justin Gent <justinbgent@gmail.com>
2020-08-03 11:14:55 -0600

    Add MovieDetails.kt and movie_details.xml to nav_graph.xml

---
+++ app/src/main/java/com/practice_project/movieapp/MovieDetails.kt
@@ -0,0 +1,32 @@
+package com.practice_project.movieapp
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.practice_project.movieapp.dagger.App
+import com.practice_project.movieapp.views.MainActivity
+
+class MovieDetails : Fragment() {
+    lateinit var mainActivity: MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+        //(mainActivity.application as App).appComponent.inject(this)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.movie_details, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
